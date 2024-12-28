5861c6cdbbfba8cd0c03fb6ac550cd89ae2261e9
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2024-12-29 02:54:45 +0530

    Fixed Error screen

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import ErrorSVG from "../assets/Images/404error.svg";
 const Error = () => {
   return (
-    <div className='flex justify-center items-center text-3xl text-white'>
-      Error - 404 Not found
+    <div className="flex flex-col justify-center h-screen items-center text-3xl text-white">
+      <img
+        src={ErrorSVG}
+        width={350}
+        height={350}
+        className="object-contain"
+        alt="Error"
+      />
+      <h1>Page Not Found</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
